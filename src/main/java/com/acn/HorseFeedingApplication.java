73dd49d8e876c3ac678e331e07200fd72a11617c
Sebastian Heming <sebastian.heming@accenture.com>
2022-06-01 11:09:00 +0200

    added starter class and packages

---
+++ src/main/java/com/acn/HorseFeedingApplication.java
@@ -0,0 +1,13 @@
+package com.acn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseFeedingApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HorseFeedingApplication.class, args);
+    }
+}
