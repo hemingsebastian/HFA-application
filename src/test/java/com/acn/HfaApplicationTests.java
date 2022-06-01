00bbc7e6ce91f22656d3204013bc41760a285e27
Sebastian Heming <sebastian.heming@accenture.com>
2022-06-01 10:54:56 +0200

    inital commit

---
+++ src/test/java/com/acn/HfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
